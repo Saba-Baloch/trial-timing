bdc8305c0cdf314f4175428a355b994a11e2fffa
Saba-Baloch <156021281+Saba-Baloch@users.noreply.github.com>
2024-03-31 01:50:40 +0430

    added index.html file

---
+++ script.js
@@ -0,0 +1,15 @@
+let hours = prompt("type here your time"); 
+hours>=24;
+hours>= "assbsjfbdjfjhdskjfhsk";
+
+if(hours <=5 ){
+    document.write("Good morning!");
+}else if (hours <= 12){
+    document.write("Good evening  ");  
+}else if ( hours<= 15){
+    document.write("Good afternoon "); 
+}else if(hours <= 24){
+   document.write("Good night");
+}else {
+    document.write("invalid time!");
+}
